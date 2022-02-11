995898b60ee8cf6798d8c80d8dc9574a05becaeb
mohitsinghrathore <singhrathoremohit@gmail.com>
2022-02-11 19:38:50 +0530

    final commit

--- app/Service/user_service/add_user.rb
+++ app/Service/user_service/add_user.rb
@@ -6,6 +6,7 @@ class UserService::AddUser<ApplicationService
     @user_params=user
   end
   def call
+    #creating the user
     user = User.new(user_params[:user])
     user.save
     group_user = GroupUser.new(user_id: user.id, group_id: user_params[:group_id][:id])
